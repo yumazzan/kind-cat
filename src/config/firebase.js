4ba6ce5979fe252bd2f0c0f853f9e54cf5c1c31f
ciera <a1@1ui-MacBookPro.local>
2025-12-25 04:08:15 +0900

    Fix Firebase database URL with correct project ID

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,10 +4,10 @@ import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
   apiKey: "AIzaSyBkO_LBcOMF2VOMywDKAP28yW7uiOvMPAs",
-  authDomain: "yuma-f4924-3ccfd.firebaseapp.com",  // ← 수정
-  databaseURL: "https://yuma-f4924-3ccfd-default-rtdb.firebaseio.com",  // ← 수정 (중요!)
-  projectId: "yuma-f4924-3ccfd",  // ← 수정
-  storageBucket: "yuma-f4924-3ccfd.firebasestorage.app",  // ← 수정
+  authDomain: "yuma-f4924-3ccfd.firebaseapp.com",
+  databaseURL: "https://yuma-f4924-3ccfd-default-rtdb.firebaseio.com",
+  projectId: "yuma-f4924-3ccfd",
+  storageBucket: "yuma-f4924-3ccfd.firebasestorage.app",
   messagingSenderId: "918394185102",
   appId: "1:918394185102:web:86181765bc32450c3f0df",
   measurementId: "G-K5R5RV2K60"
