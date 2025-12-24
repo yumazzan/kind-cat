f21833fc9f4fa12e79b9216d515e8da7088369ae
ciera <a1@1ui-MacBookPro.local>
2025-12-25 04:07:28 +0900

    Fix Firebase database URL

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,15 +2,14 @@
 import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBkO_LBcOMF2VOMywDKAP28yW7uiOvMPAs",
-  authDomain: "yuma-f4924.firebaseapp.com",
-  databaseURL: "https://yuma-f4924-default-rtdb.firebaseio.com",
-  projectId: "yuma-f4924",
-  storageBucket: "yuma-f4924.firebasestorage.app",
+  authDomain: "yuma-f4924-3ccfd.firebaseapp.com",  // ← 수정
+  databaseURL: "https://yuma-f4924-3ccfd-default-rtdb.firebaseio.com",  // ← 수정 (중요!)
+  projectId: "yuma-f4924-3ccfd",  // ← 수정
+  storageBucket: "yuma-f4924-3ccfd.firebasestorage.app",  // ← 수정
   messagingSenderId: "918394185102",
-  appId: "1:918394185102:web:861815765bc32450c3f0df",
+  appId: "1:918394185102:web:86181765bc32450c3f0df",
   measurementId: "G-K5R5RV2K60"
 };
 
